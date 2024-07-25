e79df47afb5288d132ce4f5e07f132cf71ece279
LarissaBndct <larissagomes0119@gmail.com>
2024-07-25 09:49:25 -0300

    ajustes

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,19 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama ok
+        private string Modelo { get; set;}
+        private string IMEI{ get; set;}
+        private string Memoria {get; set; }
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
+            // TODO: Passar os parâmetros do construtor para as propriedades ok
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
+
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia n = new Nokia("169999999", "790", "hgds54gg78rs46fd4g8ew","4GB");
+Iphone i = new Iphone("169888888", "12 plus", "sdf45das55fds75a","6GB");
+
+n.InstalarAplicativo("whatsapp");
+n.Ligar();
+n.ReceberLigacao();
+i.InstalarAplicativo("whatsapp");
+i.Ligar();
+i.ReceberLigacao();
